f09eafcb8d14b0fe8c23b905f3ffdaa2c087da34
zaanposni <me@zaanposni.com>
2024-01-26 16:46:18 +0100

    fix imprint link

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,3 @@
 <div class="flex justify-end px-8">
-    <a href="https://cdn.znpn.xyz/api/files/1" target="_blank" class="px-4 py-4 hover:text-red-500"> Imprint & Privacy</a>
+    <a href="https://cdn.zaanposni.com/api/files/1" target="_blank" class="px-4 py-4 hover:text-red-500"> Imprint & Privacy</a>
 </div>
